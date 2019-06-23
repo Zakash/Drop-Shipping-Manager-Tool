8be9232214f56fa1e5b11e5385b20599c4a4f9a0
zakahs <zakash@gmail.com>
2019-06-23 00:48:46 -0700

    Components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Route, RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
+import { APP_ROUTES } from './app.routes';
+import { OrdersComponent } from './orders/orders.component';
+import { OrderDetailComponent } from './order-detail/order-detail.component';
+import { AppnavComponent } from './appnav/appnav.component';
+import { FinanceComponent } from './finance/finance.component';
+import { ShippingComponent } from './shipping/shipping.component';
+import { AdminComponent } from './admin/admin.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrdersComponent,
+    OrderDetailComponent,
+    AppnavComponent,
+    FinanceComponent,
+    ShippingComponent,
+    AdminComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(APP_ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
